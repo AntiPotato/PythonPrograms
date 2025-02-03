95d0884518d505bef041f3e1b7f2afc1fb905018
AntiPotato <38892894+AntiPotato@users.noreply.github.com>
2025-02-02 20:11:24 -0800

    Update numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -1,4 +1,14 @@
-for i in $(seq 1 1000);
+date
+#for i in $(seq 1 1000000);
+#do
+    #echo $RANDOM >> file1.txt
+#done
+# Use buferring by redirecting output of  larger block to reduce the execution time.
+{
+for i in $(seq 1 1000000);
 do
-    echo $RANDOM >> file1.txt
+    echo $RANDOM 
 done
+} >> file1.txt
+date
+date -ud "@$SECONDS" "+Time elapsed: %H:%M:%S"
