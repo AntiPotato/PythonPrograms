a95b0430453cde0e47f3c1a9e3310f0dcdc0bf0c
AntiPotato <38892894+AntiPotato@users.noreply.github.com>
2025-02-02 20:18:59 -0800

    Update numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -1,14 +1,20 @@
 date
+# This simple echo method take 59 econds.
 #for i in $(seq 1 1000000);
 #do
     #echo $RANDOM >> file1.txt
 #done
-# Use buferring by redirecting output of  larger block to reduce the execution time.
-{
-for i in $(seq 1 1000000);
-do
-    echo $RANDOM 
-done
-} >> file1.txt
+
+# This method of redirecting a buffer or a lerger block takes 9 seconds
+#{
+#for i in $(seq 1 1000000);
+#do
+    #echo $RANDOM 
+#done
+#} >> file1.txt
+
+# This method of using awk and also redirecting a buffer takes 1 second.
+awk 'BEGIN{srand(); for(i=1; i<=1000000; i++) print int(rand()*1000000)}' >> file1.txt
+
 date
 date -ud "@$SECONDS" "+Time elapsed: %H:%M:%S"
